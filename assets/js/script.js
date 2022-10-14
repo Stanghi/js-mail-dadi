948b4ec7773f1440cffd0922d6859c2bc210f468
Stanghi <stangherlin.matti@gmail.com>
2022-10-14 14:56:01 +0200

    [add] JS dadi extractor and found winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
+console.log("----- MAIL -----");
 
 const listaMail = ['stanghi@gmail.com', 'lando@gmail.com', 'max@gmail.com'];
 let found = false;
@@ -33,3 +34,19 @@ else{
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
+console.log("----- DADI -----");
+
+const numberUser = Math.ceil(Math.random() * 6);
+const numberPC = Math.ceil(Math.random() * 6);
+console.log("USER",numberUser);
+console.log("PC",numberPC);
+
+if (numberUser > numberPC){
+    console.log("User vince");
+}
+else if (numberUser < numberPC){
+    console.log("PC vince");
+}
+else{
+    console.log("Patta");
+}
